f9d3207d19a2ad309060ae4fb36062b0db7e5a3c
LeonRivera <leonriv2@gmail.com>
2022-08-03 00:39:25 -0500

    03082022 | LeonRivera | Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormUsersComponent } from './form-users/form-users.component';
 import { CardsProductsComponent } from './cards-products/cards-products.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { HeaderComponent } from './header/header.component';
     FormUsersComponent,
     CardsProductsComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
